c8f1e70bd200e7276132723b1a419db9600fd4de
Priyanshu T <priyanshu9943@gmail.com>
2023-01-04 16:31:36 +0530

    init

--- Day 5/Practice/009.cpp
+++ Day 5/Practice/009.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
-  
+  int size, cell;
+  cin >> size;
+
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      cin >> cell;
+      if (cell != 20 && cell > 10) {
+        cout << "No";
+        return 0;
+      }
+    }
+  }
+
+  cout << "Yes";
 
   return 0;
 }
--- Day 5/Practice/010.cpp
+++ Day 5/Practice/010.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
-  
+  int size, cell;
+  cin >> size;
+
+  int count = 0;
+
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      cin >> cell;
+      if (cell != 20)
+        count++;
+    }
+  }
+
+  cout << count;
 
   return 0;
 }
