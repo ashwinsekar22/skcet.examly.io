6553e16377cb1ea2f46e588bd1851277790cd3cf
Priyanshu T <priyanshu9943@gmail.com>
2023-01-07 12:57:27 +0530

    Day 5: Practice - 005.cpp changed

--- Day 5/Practice/004.cpp
+++ Day 5/Practice/004.cpp
@@ -1,8 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main() {
-  
+int main()
+{
+  int size;
+  cin >> size;
 
-  return 0;
+  int arr[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    cin >> arr[i];
+  }
+
+  for (int i = 0; i < size; i++)
+  {
+    int count = 1;
+    for (int j = 0; j < size; j++)
+    {
+      if (arr[i] == arr[j] && i != j)
+      {
+        count++;
+      }
+    }
+
+    if (count % 2 != 0)
+    {
+      cout << arr[i];
+      break;
+    }
+  }
 }
